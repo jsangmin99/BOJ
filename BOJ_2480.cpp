88bb403cf2d2a1025362c013e7dea611d3fa1e73
jsangmin99 <jsangmin99@gmail.com>
2022-02-15 20:19:19 +0900

    검증수

---
+++ BOJ_2480.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+ 
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+ 
+	vector<int> v(3);
+	cin >> v[0] >> v[1] >> v[2];
+ 
+	sort(v.begin(), v.end());
+	if (v[0] == v[1] && v[1] == v[2]) cout << 10000 + (v[0] * 1000);
+	else if (v[0] == v[1]) cout << 1000 + (v[0] * 100);
+	else if (v[0] == v[2]) cout << 1000 + (v[0] * 100);
+	else if (v[1] == v[2]) cout << 1000 + (v[1] * 100);
+	else cout << v[2] * 100;
+ 
+	return 0;
+}
