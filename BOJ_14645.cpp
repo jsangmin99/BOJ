0498367b4ea658373461a4c7542669fd1272ecf4
jsangmin99 <jsangmin99@gmail.com>
2022-08-31 20:19:19 +0900

    와이버스 부릉부릉

---
+++ BOJ_14645.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int N,K, in,out;
+    cin>> N>>K;
+    for(int i=0; i<N;i++){
+        cin >>in>>out;
+    }
+    cout<< "ºñ¿ÍÀÌ";
+    
+}
