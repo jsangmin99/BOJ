49a42d6cf4056e8af2ff8b9998584cfacdfe57a9
jsangmin99 <jsangmin99@gmail.com>
2022-09-15 20:19:19 +0900

    새싹

---
+++ BOJ_25083.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    /* code */
+    cout<<"         ,r'\"7\n";
+    cout<<"r`-_   ,'  ,/\n";
+    cout << " \\. \". L_r'\n";
+    cout << "   `~\\/\n";
+    cout << "      |\n";
+    cout << "      |\n";
+
+    return 0;
+}
+/*
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+*/
