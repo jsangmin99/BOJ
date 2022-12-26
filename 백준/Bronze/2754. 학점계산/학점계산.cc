7092910535c7ec659bf3bca929afcc30e35c68c9
MoonDokkebi <95060314+MoonDokkebi@users.noreply.github.com>
2022-12-26 20:35:42 +0900

    [Bronze V] Title: 학점계산, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string credit;
+    cin >> credit;
+    if (credit == "A+") cout <<"4.3";
+    if (credit == "A0") cout <<"4.0";
+    if (credit == "A-") cout <<"3.7";
+    if (credit == "B+") cout <<"3.3";
+    if (credit == "B0") cout <<"3.0";
+    if (credit == "B-") cout <<"2.7";
+    if (credit == "C+") cout <<"2.3";
+    if (credit == "C0") cout <<"2.0";
+    if (credit == "C-") cout <<"1.7";
+    if (credit == "D+") cout <<"1.3";
+    if (credit == "D0") cout <<"1.0";
+    if (credit == "D-") cout <<"0.7";
+    if (credit == "F") cout <<"0.0";
+    
+}
