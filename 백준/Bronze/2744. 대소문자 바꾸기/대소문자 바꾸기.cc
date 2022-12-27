bd51e6903d1cc563645e31176371af1162852066
MoonDokkebi <95060314+MoonDokkebi@users.noreply.github.com>
2022-12-27 18:55:01 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] < 97)
+        {
+            cout << char(s[i] + 32);
+        }
+        else
+        {
+            cout << char(s[i] - 32);
+        }
+    }
+}
