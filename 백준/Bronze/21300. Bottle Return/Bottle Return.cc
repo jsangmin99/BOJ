77920045e23c327dc9db2ba78b71ef12170e2b7a
MoonDokkebi <95060314+MoonDokkebi@users.noreply.github.com>
2023-01-02 14:35:26 +0900

    [Bronze V] Title: Bottle Return, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21300. Bottle Return/Bottle Return.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a1,a2,a3,a4,a5,a6;
+    cin >> a1 >> a2 >> a3 >> a4 >> a5 >>a6;
+    cout << (a1+a2+a3+a4+a5+a6)*5;
+
+    return 0;
+}
