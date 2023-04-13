4c58f165ecff8a88badcb2568b42fe2dfad30ae1
MoonDokkebi <95060314+MoonDokkebi@users.noreply.github.com>
2023-04-14 00:25:25 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.cc
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.cc
@@ -1,12 +1,7 @@
-
 #include <iostream>
 using namespace std;
-
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
-	long long N;
-    cin >> N;
-	cout << N * N << '\n' << 2;
+int main(){
+    long long n;
+    cin >> n;
+    cout << n*n << "\n" << 2;
 }
