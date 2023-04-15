41af3e6c33176907f8f0917f82805dde6a91dff7
MoonDokkebi <jsangmin99@gmail.com>
2023-04-16 02:16:04 +0900

    .

--- 백준/Bronze/4562. No Brainer/No Brainer.cc
+++ 백준/Bronze/4562. No Brainer/No Brainer.cc
@@ -1,14 +1,21 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int n,a,b;
+int main()
+{
+    int n, a, b;
     cin >> n;
-    for(int i=0; i<n; i++){
+    for (int i = 0; i < n; i++)
+    {
         cin >> a >> b;
-        if(a<b){
-            cout << "NO BRAINS"<<"\n";
-        }else{
-            cout << "MMM BRAINS" << "\n";
+        if (a < b)
+        {
+            cout << "NO BRAINS"
+                 << "\n";
+        }
+        else
+        {
+            cout << "MMM BRAINS"
+                 << "\n";
         }
     }
 }
