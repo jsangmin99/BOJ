204702169683ab1c2333425ad91cc3766b4869fe
Nox <95060314+jsangmin99@users.noreply.github.com>
2023-08-13 22:17:20 +0900

    [unrated] Title: 공백으로 구분하기 2, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(string my_string) {
+    vector<string> answer;
+    string word = "";
+    
+    for(char c : my_string){
+        if(c != ' '){
+            word += c;
+        }else{
+            if(!word.empty()){
+                answer.push_back(word);
+                word = "";
+            }
+        }
+    }
+    if(!word.empty())
+        answer.push_back(word);
+    return answer;
+}
