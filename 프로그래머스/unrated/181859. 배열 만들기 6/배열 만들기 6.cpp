41009366113948177cedec687244d2e0ab7e8845
Nox <95060314+jsangmin99@users.noreply.github.com>
2023-08-17 16:27:01 +0900

    [unrated] Title: 배열 만들기 6, Time: 1.07 ms, Memory: 6.54 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    vector<int> stk;
+    for( int i =0; i < arr.size(); i++){
+        if(!stk.empty() && stk.back() == arr[i]){
+            stk.pop_back();
+        }
+        else{
+            stk.push_back(arr[i]);
+        }
+    }
+    if(stk.empty()){
+        stk.push_back(-1);
+    }
+    return stk;
+}
