df35366c69e82dca72d96ab57782349a37da282c
Nox <95060314+jsangmin99@users.noreply.github.com>
2023-08-18 00:44:14 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    string st ="";
+    int n = 0;
+    
+    cin >> st;
+    cin >> n;
+
+    cout << st[n-1];
+    
+    return 0;
+}
