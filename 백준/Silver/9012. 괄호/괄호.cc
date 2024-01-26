7a657d94657796090c4ea025559beea7c7af228d
Nox <95060314+jsangmin99@users.noreply.github.com>
2024-01-26 09:18:36 +0900

    [Silver IV] Title: 괄호, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<stack>
+#include<string>
+using namespace std;
+int main() {
+	int cnt = 0;
+	int flag = 0; // true/false 판단을 위한 flag
+	string str;
+ 
+	cin >> cnt;
+	for (int i = 0; i < cnt; i++) {
+		stack<char> s;
+		cin >> str;
+		for (int j = 0; j < str.size(); j++) {
+			if (str[j] == '(') {
+				s.push(str[j]);
+			}
+			else if (str[j] == ')') { 
+				if (s.empty()) { 
+					flag = 1;
+					break;
+				}
+				else {
+					s.pop();
+				}
+			}
+        }
+		if (!s.empty() || flag == 1) { 
+			cout << "NO" << endl;    
+		}
+		else {
+			cout << "YES" << endl;
+		}
+		flag = 0; 
+	}
+	return 0;
+}
