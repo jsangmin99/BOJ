3a4270ca6ad07cae16d8a217b0e0958905574733
Nox <95060314+jsangmin99@users.noreply.github.com>
2024-04-11 14:44:55 +0900

    [level 2] Title: 방문 길이, Time: 0.08 ms, Memory: 3.66 MB -BaekjoonHub

---
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <map>
+
+using namespace std;
+
+int solution(string dirs) {
+    int answer = 0;
+    map<pair<pair<int,int>, pair<int,int>>, bool> M;
+    int now_x=0, now_y=0,next_x=0, next_y=0;
+    for(int i = 0; i < dirs.size(); i++){
+        if(dirs[i] == 'U' && next_y+1 <= 5){ //단 좌표평면의 경계를 넘어가는 명령어는 무시한다.
+            next_y++;
+        }else if(dirs[i] == 'D' && next_y-1 >= -5){
+            next_y--;
+        }else if (dirs[i] == 'R' && next_x+1 <= 5){
+            next_x++;
+        }else if(dirs[i] == 'L' && next_x-1 >= -5){
+            next_x--;
+        }else{
+            continue;
+        }
+        if(M.find(make_pair(make_pair(now_x,now_y),make_pair(next_x,next_y))) == M.end()
+          && M.find(make_pair(make_pair(next_x,next_y),make_pair(now_x,now_y)))==M.end()) {
+            
+            M.insert({make_pair(make_pair(now_x,now_y),make_pair(next_x,next_y)), true});
+            answer++;// 현재좌표와 다음좌표 등록
+    
+        }
+        now_x= next_x; now_y = next_y;
+    }
+    return answer;
+}
