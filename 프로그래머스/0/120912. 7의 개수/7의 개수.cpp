f28f4e5b9791528833a69362d467c988f0941dd0
전상민 <95060314+jsangmin99@users.noreply.github.com>
2024-04-15 16:14:53 +0900

    [level 0] Title: 7의 개수, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> array) {
+    int answer = 0;
+    for(int i : array){
+        while(i > 0) {
+            if(i % 10 == 7) 
+                answer++;
+            i /= 10;
+        }
+    }
+    return answer;
+}
