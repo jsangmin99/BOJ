57f42db88b44b74b7aff75f9fcf8ca6b987ca571
전상민 <95060314+jsangmin99@users.noreply.github.com>
2024-04-16 13:06:20 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.02 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    string s;
+    for(int i = 0; i < n; i++){
+        answer++;
+        s = to_string(answer);
+        while(answer % 3 == 0 || s.find('3') != string::npos){
+            answer++;
+            s = to_string(answer);
+        }
+    }    
+    return answer;
+}
