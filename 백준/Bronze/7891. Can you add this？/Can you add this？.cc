685c9f7ce9824733226670811363263424f316d5
전상민 <95060314+jsangmin99@users.noreply.github.com>
2024-04-25 13:40:45 +0900

    [Bronze V] Title: Can you add this?, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n =0;;
+    cin >> n;
+    for(int i = 0; i < n; i ++){
+        int a= 0, b=0;
+        cin >> a >> b;
+        cout << a+b << "\n";
+    }
+    
+}
