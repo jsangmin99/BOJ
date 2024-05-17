1f41ec462f0240c94528d71c6fcf00c0595a23d3
전상민 <95060314+jsangmin99@users.noreply.github.com>
2024-05-17 09:23:24 +0900

    [level 2] Title: 삼각 달팽이, Time: 171.42 ms, Memory: 111 MB -BaekjoonHub

---
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n) {
+    vector<int> answer;
+    int snail[1001][1001];
+    int x = 0, y = 0, state = 0, num = 1;
+    for(int i = 0; i<n; i++){
+        if(state == 0){ //아래로 내려갈떄
+            for(int j = i; j < n; j++)
+                snail[x++][y] = num++;
+            x--; y++;
+            state = 1;
+            continue;
+        }
+        
+        if(state == 1){ // 오른쪽으로 갈떄
+            for(int j = i; j < n; j++)
+                snail[x][y++] = num++;
+            x--; y-=2;
+            state = 2;
+            continue;
+        }
+        
+        if(state == 2){ //대각선 위
+            for(int j = i; j < n; j++)
+                snail[x--][y--] = num++;
+            x+=2; y++;
+            state = 0;
+        }
+    }
+     for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            if(snail[i][j]) answer.push_back(snail[i][j]);
+    return answer;
+}
