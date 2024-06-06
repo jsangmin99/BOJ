b6125c474c12990b566e0988dccd0d5067bf6560
전상민 <95060314+jsangmin99@users.noreply.github.com>
2024-06-06 17:36:43 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    cin >>s ;
+    if (s ==  "SONGDO")
+        cout << "HIGHSCHOOL";
+    if (s== "CODE")
+        cout <<"MASTER";
+    if(s == "2023")
+        cout << "0611";
+    if (s== "ALGORITHM")
+        cout << "CONTEST";
+    
+    return 0;
+}
