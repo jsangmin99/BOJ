d15aa5d9ea656ce52399b6d8a29600ad74cd9390
전상민 <95060314+jsangmin99@users.noreply.github.com>
2024-06-07 13:38:19 +0900

    [Bronze V] Title: An Easy-Peasy Problem, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30214. An Easy－Peasy Problem/An Easy－Peasy Problem.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    if(n >= (m-n)){
+        cout << "E";
+    }else{
+        cout << "H";
+    }
+
+
+return 0;
+}
