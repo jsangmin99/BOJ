62cb9d99a7393e0dc7794de367d2654e96f1fc11
전상민 <95060314+jsangmin99@users.noreply.github.com>
2024-06-07 14:59:56 +0900

    [level 0] Title: 문자열 밀기, Time: 1.86 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/README.md
+++ 프로그래머스/0/120921. 문자열 밀기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 밀기 - 120921 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120921?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120921) 
 
 ### 성능 요약
 
-메모리: 76 MB, 시간: 1.11 ms
+메모리: 74.6 MB, 시간: 1.86 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 07일 14:34:20
+2024년 06월 07일 14:59:54
 
 ### 문제 설명
 
--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,18 +1,5 @@
 class Solution {
     public int solution(String A, String B) {
-        int length = A.length();
-        if (A.equals(B)) {
-            return 0;
-        }
-        
-        String s = A;
-        for (int i = 1; i < length; i++) {
-            s = s.substring(length - 1) + s.substring(0, length - 1);
-            if (s.equals(B)) {
-                return i;
-            }
-        }
-        
-        return -1;
+      return (B+B).indexOf(A);
     }
 }
