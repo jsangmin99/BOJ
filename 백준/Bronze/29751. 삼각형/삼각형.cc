91cddd243607f8d7950a2bace0894ebba4fdcee0
전상민 <95060314+jsangmin99@users.noreply.github.com>
2024-06-07 09:22:01 +0900

    [Bronze V] Title: 삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double n, m;
+    cin >> n >> m;
+    cout<<fixed;
+    cout.precision(1);
+    cout << n*m/2;
+    
+    return 0;
+}
