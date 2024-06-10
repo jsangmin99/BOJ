04b16f173f3347b3daf33bafa997a2c99d3f88d8
전상민 <95060314+jsangmin99@users.noreply.github.com>
2024-06-10 17:25:28 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 1.72 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -1,31 +1,19 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] numbers) {
-        int[] answer = {};
-        int[] tmp = new int[numbers.length * numbers.length];
-
-        int count = 0;
-
-        for(int i = 0; i < numbers.length; i++){
-            for (int j = i+1; j < numbers.length; j++){
-                tmp[count] = numbers[i] + numbers[j];
-                count++;
+        List<Integer> answerList = new ArrayList<>();
+        for(int i = 0; i < numbers.length; i++) {
+            for (int j = i+1; j < numbers.length; j++) {
+                if (!answerList.contains(numbers[i] + numbers[j])) {
+                    answerList.add(numbers[i] + numbers[j]);
+                }
             }
         }
-        tmp = Arrays.copyOf(tmp, count);
-        int[] newtmp = new int[tmp.length];
-        Arrays.sort(tmp);
-        int num = 0;
-        for(int i = 0; i < tmp.length; i++){
-            if(i == 0 || tmp[i] != tmp[i-1]){
-                newtmp[num] = tmp[i];
-                num++;
-            }
+        answerList.sort(Comparator.naturalOrder());
+        int answer[] = new int[answerList.size()];
+        for (int i = 0; i < answerList.size(); i++) {
+            answer[i] = answerList.get(i);
         }
-        answer = Arrays.copyOfRange(newtmp, 0, num);
-    
-
-
         return answer;
     }
 }
