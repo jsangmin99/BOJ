89e1408381d350ddb8d2a8afc56d2119b345ac69
전상민 <95060314+jsangmin99@users.noreply.github.com>
2024-06-21 17:13:34 +0900

    [level 1] Title: 명예의 전당 (1), Time: 3.24 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -1,22 +1,17 @@
 import java.util.*;
 class Solution {
     public int[] solution(int k, int[] score) {
-            int[] answer = {};
-            PriorityQueue<Integer> pq = new PriorityQueue<>();
-            List<Integer> list = new ArrayList<>();
-
-            for(int i = 0 ; i < score.length; i++) {
-                int temp = 0;
-                pq.add(score[i]);
-
-                if(pq.size() > k){
-                    pq.remove();
-                    list.add(pq.peek());
-                }else{
-                    list.add(pq.peek());
-                }
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        List<Integer> list = new ArrayList<>();
+        for(int i : score){
+            pq.add(i);
+            if(pq.size() > k){
+                pq.remove();
+                list.add(pq.peek());
+            }else{
+                list.add(pq.peek());
             }
-            answer = list.stream().mapToInt(i -> i).toArray();
-            return answer;
+        }
+        return list.stream().mapToInt(i -> i).toArray();
         }
 }
