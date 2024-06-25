e406b491537ed1ce2ec875d7f9c248ae511f6ce8
전상민 <95060314+jsangmin99@users.noreply.github.com>
2024-06-25 10:19:51 +0900

    [level 3] Title: 네트워크, Time: 0.47 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -1,15 +1,14 @@
-#include <string>
-#include <vector>
-
+#include<iostream>
+#include<vector>
 using namespace std;
-int check[200];
-void dfs(vector<vector<int>> computers, int n, int current_node){
-    check[current_node] = 1;
+int check[201];
+void dfs(vector<vector<int>> computers, int n, int visited){
+    check[visited] = 1;
   	for(int j=0; j<n; j++) { 
-    	if(check[j] == 0 && computers[current_node][j] == 1) {   
+    	if(check[j] == 0 && computers[visited][j] == 1) {   
       		dfs(computers,n,j);
         }
-    }   
+    } 
 }
 
 int solution(int n, vector<vector<int>> computers) {
@@ -22,3 +21,4 @@ int solution(int n, vector<vector<int>> computers) {
     }
     return answer;
 }
+
