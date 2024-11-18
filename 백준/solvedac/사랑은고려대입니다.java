e967526116cdaf31d04d69049040e38abfabb761
jsangmin99 <jsangmin99@gmail.com>
2024-11-18 14:27:57 +0900

    사랑은고려대입니다.

---
+++ 백준/solvedac/사랑은고려대입니다.java
@@ -0,0 +1,21 @@
+package solvedac;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+
+public class 사랑은고려대입니다 {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final String word = "LoveisKoreaUniversity";
+        
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < n; i++){
+            System.out.print(word);
+            System.out.print(" ");
+        }
+    }
+}
