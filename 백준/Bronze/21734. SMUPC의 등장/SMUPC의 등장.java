9283b5e415fb91aed1efa8f89211809338647fcb
전상민 <95060314+jsangmin99@users.noreply.github.com>
2024-12-02 16:09:02 +0900

    [Bronze II] Title: SMUPC의 등장, Time: 176 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/21734. SMUPC의 등장/SMUPC의 등장.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String S = sc.nextLine();
+        for (int i = 0; i < S.length(); i++) {
+            char c = S.charAt(i);
+            int asciValue = (int) c;
+            int digitSum = sumOfDigits(asciValue);
+
+            for(int j =0; j <digitSum; j++){
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
+
+    private static int sumOfDigits(int num) {
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
+    }
+}
