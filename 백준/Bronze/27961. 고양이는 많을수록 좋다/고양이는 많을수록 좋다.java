3ed01f696238be6b8a3f60e261cd871b1438e9d9
전상민 <95060314+jsangmin99@users.noreply.github.com>
2024-12-02 16:45:17 +0900

    [Bronze I] Title: 고양이는 많을수록 좋다, Time: 108 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/27961. 고양이는 많을수록 좋다/고양이는 많을수록 좋다.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws Exception {
+        long n = Long.parseLong(br.readLine());
+        int cnt = 0;
+        while (n > 3) {
+            cnt++;
+            n = n/2 + (n%2==1?1:0);
+        }
+        System.out.println(cnt + n);
+    }
+}
