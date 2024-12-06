6fd0303c6c02939f5a5f7d3e06b3a10d79b06476
전상민 <95060314+jsangmin99@users.noreply.github.com>
2024-12-06 16:59:53 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 100 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = 0; i < 10; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int output = Integer.parseInt(st.nextToken());
+            int input = Integer.parseInt(st.nextToken());
+            sum = sum + input - output;
+            if (sum >= max) {
+                max = sum;
+            }
+        }
+
+        System.out.println(max);
+    }
+}
