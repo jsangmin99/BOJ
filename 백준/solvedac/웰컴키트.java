3817281ad8246333df6854a07deb9342295015aa
jsangmin99 <jsangmin99@gmail.com>
2024-12-06 21:03:28 +0900

    웰컴키트

---
+++ 백준/solvedac/웰컴키트.java
@@ -0,0 +1,38 @@
+package solvedac;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 웰컴키트 {
+    // 첫째중 N 참가자의수
+    // 사이즈별 신청자수
+    // 티셔츠 tn T 펜의 묶음수 P
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int arr[] = new int[6];
+
+        int result1 = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 6; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < 6; i++) {
+            if (arr[i] % T == 0) {
+                result1 += arr[i] / T;
+            } else {
+                result1 += arr[i] / T + 1;
+            }
+        }
+        System.out.println(result1);
+        System.out.println(N / P + " " + N % P);
+
+    }
+}
