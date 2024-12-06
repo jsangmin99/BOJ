b628cf488dbfa13a5b711d5e5c20342bb17af5be
jsangmin99 <jsangmin99@gmail.com>
2024-12-06 17:51:07 +0900

    N번째 큰 수

---
+++ 백준/MyBoj/N번째큰수.java
@@ -0,0 +1,28 @@
+package MyBoj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class N번째큰수 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            while (st.hasMoreTokens()) {
+                int num = Integer.parseInt(st.nextToken());
+                pq.add(num);  
+                if (pq.size() > n) {  
+                    pq.poll();
+                }
+            }
+        }
+
+        System.out.println(pq.poll());
+    }
+}
