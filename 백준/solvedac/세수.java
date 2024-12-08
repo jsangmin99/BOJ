9ed2c00bd7b5a6ed56769ec0d575d6bf795f6a5e
jsangmin99 <jsangmin99@gmail.com>
2024-12-08 13:26:58 +0900

    세수

---
+++ 백준/solvedac/세수.java
@@ -0,0 +1,23 @@
+package solvedac;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.Arrays;
+
+public class 세수 {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[3];
+        String[] input = br.readLine().split(" ");
+        arr[0] = Integer.parseInt(input[0]);
+        arr[1] = Integer.parseInt(input[1]);
+        arr[2] = Integer.parseInt(input[2]);
+
+        Arrays.sort(arr);
+
+        System.out.println(arr[1]);
+    }
+}
