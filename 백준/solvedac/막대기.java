c7b0a8764bf77e052514ac91391f8fc50e49b82b
jsangmin99 <jsangmin99@gmail.com>
2024-12-10 15:39:13 +0900

    막대기

---
+++ 백준/solvedac/막대기.java
@@ -0,0 +1,24 @@
+package solvedac;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+
+public class 막대기 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int count = 0;
+
+        while (n > 0) {
+            if (n % 2 == 1) {
+                count++;
+            }
+            n /= 2;
+
+        }
+        System.out.println(count);
+
+    }
+}
