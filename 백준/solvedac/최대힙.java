b4f0ebaff435f982c5482fc78cb739f387f2dd63
jsangmin99 <jsangmin99@gmail.com>
2024-12-10 16:34:49 +0900

    최대힙

---
+++ 백준/solvedac/최대힙.java
@@ -0,0 +1,33 @@
+package solvedac;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class 최대힙 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Queue pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if (x == 0) {
+                if (pq.size() == 0) {
+                    System.out.println("0");
+                } else {
+                    System.out.println(pq.poll());
+                }
+
+            } else {
+                pq.offer(x);
+            }
+
+        }
+    }
+
+}
