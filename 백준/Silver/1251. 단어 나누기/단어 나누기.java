eb4fffa43dce84686e70dbe67ac4ac63b9e643f8
전상민 <95060314+jsangmin99@users.noreply.github.com>
2024-12-11 22:37:13 +0900

    [Silver V] Title: 단어 나누기, Time: 212 ms, Memory: 19372 KB -BaekjoonHub

---
+++ 백준/Silver/1251. 단어 나누기/단어 나누기.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.nextLine();
+        scanner.close();
+
+        String result = null;
+
+        // 단어를 세 부분으로 나누기
+        for (int i = 1; i < word.length() - 1; i++) {
+            for (int j = i + 1; j < word.length(); j++) {
+                String part1 = word.substring(0, i);
+                String part2 = word.substring(i, j);
+                String part3 = word.substring(j);
+
+                // 각 부분 뒤집기
+                String reversed = reverse(part1) + reverse(part2) + reverse(part3);
+
+                // 사전순으로 가장 앞서는 단어 찾기
+                if (result == null || reversed.compareTo(result) < 0) {
+                    result = reversed;
+                }
+            }
+        }
+
+        System.out.println(result);
+    }
+
+    // 문자열 뒤집기
+    private static String reverse(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
+    
+}
