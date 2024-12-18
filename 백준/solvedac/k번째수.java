86fb4cb839c7e242ac0df926d06b640ae267cc0c
jsangmin99 <jsangmin99@gmail.com>
2024-12-18 22:25:43 +0900

    k번쨰수

---
+++ 백준/solvedac/k번째수.java
@@ -0,0 +1,31 @@
+package solvedac;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class k번째수 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a); 
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            maxHeap.offer(num);
+
+            if (maxHeap.size() > k) {
+                maxHeap.poll();
+            }
+        }
+
+        System.out.println(maxHeap.peek());
+    }
+    
+}
