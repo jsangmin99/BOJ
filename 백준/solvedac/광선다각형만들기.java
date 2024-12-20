10f2516a81ed176db2a9d79856b89bf5675dd28e
jsangmin99 <jsangmin99@gmail.com>
2024-12-20 22:18:00 +0900

    광선 다각형 만들기

---
+++ 백준/solvedac/광선다각형만들기.java
@@ -0,0 +1,25 @@
+package solvedac;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 광선다각형만들기 {
+     public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());  
+        int sum = 0;
+        
+        // 각 반사각도 받아서 합산
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int tmp = Integer.parseInt(st.nextToken());
+            sum += tmp * 2;
+        }
+
+        int total = (n-1) * 180;
+
+        int result = (total - sum);
+        System.out.println(result);
+    }
+}
