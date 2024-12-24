b11956b7b8b4ab4816693bc066bbc97fc7e66542
전상민 <95060314+jsangmin99@users.noreply.github.com>
2024-12-24 15:25:07 +0900

    [Bronze III] Title: 핸드폰 요금, Time: 124 ms, Memory: 15828 KB -BaekjoonHub

---
+++ 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            arr.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int youngsikCount = 0;
+        int minsikCount = 0;
+        for (int i : arr) {
+            if (i % 30 >= 0) {
+                youngsikCount += 10;
+            }
+            youngsikCount += i / 30 * 10;
+
+            if (i % 60 >= 0) {
+                minsikCount += 15;
+            }
+            minsikCount += i / 60 * 15;
+        }
+
+
+        // 출력
+        if (minsikCount > youngsikCount) {
+            System.out.println("Y " + youngsikCount);
+        } else if (minsikCount < youngsikCount) {
+            System.out.println("M " + minsikCount);
+        } else {
+            System.out.println("Y M " + minsikCount);
+        }
+
+    }
+}
