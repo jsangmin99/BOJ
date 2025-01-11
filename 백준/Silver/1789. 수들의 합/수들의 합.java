c4022333fcac64fcc187670ee80d5d709a525c48
전상민 <95060314+jsangmin99@users.noreply.github.com>
2025-01-11 20:30:26 +0900

    [Silver V] Title: 수들의 합, Time: 176 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Silver/1789. 수들의 합/수들의 합.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+//        N 까지의 합은
+//        N * (N + 1) / 2
+        long S = 0; //입력값
+        long N = 0; // 자연수의 갯수
+        long sum = 0; // 합
+
+        Scanner sc = new Scanner(System.in);
+        S = sc.nextLong();
+
+//         1부터 순차적으로 더하며 합이 S를 넘지않도록 체크
+        while (sum + (N + 1) <= S) {
+            N++;
+            sum += N;
+        }
+        System.out.println(N);
+    }
+}
