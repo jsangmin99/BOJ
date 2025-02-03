5d128ce36fd8bbbee62e9e24a1736a6571e536c7
전상민 <95060314+jsangmin99@users.noreply.github.com>
2025-02-03 14:50:59 +0900

    [Silver IV] Title: 토너먼트, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Silver/1057. 토너먼트/토너먼트.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        int count = 0;
+
+        while (A != B) {
+            A = (A + 1) / 2;
+            B = (B + 1) / 2;
+            count++;
+        }
+
+        System.out.println(count);
+    }
+}
+
+
