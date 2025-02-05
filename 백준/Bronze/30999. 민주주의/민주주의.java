33eff2ddbfb8028373c694c75454d7e6485267cb
전상민 <95060314+jsangmin99@users.noreply.github.com>
2025-02-05 14:42:10 +0900

    [Bronze IV] Title: 민주주의, Time: 104 ms, Memory: 14080 KB -BaekjoonHub

---
+++ 백준/Bronze/30999. 민주주의/민주주의.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            int count = 0;
+            for (int j = 0; j < m; j++) {
+                if (s.charAt(j) == 'O') {
+                    count++;
+                }
+            }
+            if (m / 2 < count) {
+                result++;
+            }
+        }
+        System.out.println(result);
+
+
+    }
+}
