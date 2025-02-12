667abff29fa488dc9ce228df13abede7a61fae61
전상민 <95060314+jsangmin99@users.noreply.github.com>
2025-02-12 23:31:08 +0900

    [Silver V] Title: 기술 연계마스터 임스, Time: 208 ms, Memory: 19016 KB -BaekjoonHub

---
+++ 백준/Silver/25497. 기술 연계마스터 임스/기술 연계마스터 임스.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+
+        Stack<Character> L_stack = new Stack<>();
+        Stack<Character> S_stack = new Stack<>();
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+
+            if (Character.isDigit(ch)) {
+                count++;
+            } else if (ch == 'L') {
+                L_stack.push('L');
+            } else if (ch == 'S') {
+                S_stack.push('S');
+            } else if (ch == 'R') {
+                if (!L_stack.isEmpty()) {
+                    L_stack.pop();
+                    count++;
+                }else {
+                    break;
+                }
+            } else if (ch == 'K') {
+                if (!S_stack.isEmpty()) {
+                    S_stack.pop();
+                    count++;
+                }else {
+                    break;
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+}
