a1f1cafc7f767b3f869a32150e0fc44ca7828a36
전상민 <95060314+jsangmin99@users.noreply.github.com>
2025-02-14 03:00:50 +0900

    [Silver IV] Title: 트리 만들기, Time: 100 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Silver/14244. 트리 만들기/트리 만들기.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int cnt = 0;
+        int node = 0;
+        if(m == 2){
+            cnt = 1;
+        }
+
+        for(int i=1; i<n; i++){
+            if(m > cnt) {
+                sb.append("0").append(" ").append(i).append("\n");
+                cnt++;
+            }
+            else{
+                sb.append(node).append(" ").append(i).append("\n");
+            }
+            node = i;
+        }
+
+        System.out.println(sb);
+    }
+}
