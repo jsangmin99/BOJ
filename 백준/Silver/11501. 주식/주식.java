e2a1bcfcfff7b1aea40a46760957d8547d7db09e
전상민 <95060314+jsangmin99@users.noreply.github.com>
2025-02-15 15:59:36 +0900

    [Silver II] Title: 주식, Time: 1076 ms, Memory: 318808 KB -BaekjoonHub

---
+++ 백준/Silver/11501. 주식/주식.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int n;
+        int[] arr;
+
+        for (int i = 0; i < T; i++) {
+            n = Integer.parseInt(br.readLine());
+            arr = new int[n];
+            st = new StringTokenizer(br.readLine());
+
+            for (int j = 0; j < n; j++) {
+                arr[j] = Integer.parseInt(st.nextToken());
+            }
+
+            long max = 0;
+            int nowMax = 0;
+            for (int j = n - 1; j >= 0; j--) {
+                if (arr[j] > nowMax) {
+                    nowMax = arr[j];
+                }else {
+                    max += (nowMax - arr[j]);
+                }
+            }
+
+            sb.append(max).append("\n");
+        }
+        System.out.println(sb);
+
+    }
+}
