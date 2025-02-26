df78bbafd7d3dba986eb21a3b4e4132b75bd179d
전상민 <95060314+jsangmin99@users.noreply.github.com>
2025-02-26 15:10:47 +0900

    [Silver V] Title: 자동차 주차, Time: 104 ms, Memory: 14088 KB -BaekjoonHub

---
+++ 백준/Silver/30993. 자동차 주차/자동차 주차.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        System.out.println(factorial(n) / (factorial(a) * factorial(b) * factorial(c)));
+
+    }
+
+    public static long factorial(int num) {
+        long result = 1;
+        for (int i = 2; i <= num; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
