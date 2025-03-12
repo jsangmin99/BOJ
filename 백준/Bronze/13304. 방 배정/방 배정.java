0b54c98f0f41c360401e5e850a62368a4f9ffeb0
전상민 <95060314+jsangmin99@users.noreply.github.com>
2025-03-12 15:37:38 +0900

    [Bronze II] Title: 방 배정, Time: 228 ms, Memory: 20976 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/13304. 방 배정/방 배정.java
+++ 백준/Bronze/13304. 방 배정/방 배정.java
@@ -9,26 +9,26 @@ public class Main {
         int K = scanner.nextInt(); // 한 방의 최대 인원 수
         
         // 학년과 성별을 기반으로 그룹화된 학생 수를 저장할 배열
-        int[][] students = new int[3][2]; // (1-2학년, 3-4학년, 5-6학년) * (여, 남)
+        int[][] students = new int[6][2]; // (학년 1~6) * (여, 남)
         
         for (int i = 0; i < N; i++) {
             int gender = scanner.nextInt(); // 0: 여학생, 1: 남학생
             int grade = scanner.nextInt();
-            
-            if (grade <= 2) {
-                students[0][gender]++;
-            } else if (grade <= 4) {
-                students[1][gender]++;
-            } else {
-                students[2][gender]++;
-            }
+            students[grade - 1][gender]++;
         }
         
         // 최소 방 개수 계산
         int rooms = 0;
-        for (int i = 0; i < 3; i++) {
+        
+        // 1~2학년은 성별 관계없이 함께 배정 가능
+        int lowerGradeStudents = students[0][0] + students[0][1] + students[1][0] + students[1][1];
+        rooms += (lowerGradeStudents + K - 1) / K;
+        
+        // 3~4학년, 5~6학년은 성별 별도 배정
+        for (int i = 2; i < 6; i += 2) {
             for (int j = 0; j < 2; j++) {
-                rooms += (students[i][j] + K - 1) / K; // 올림 처리하여 필요한 방 수 계산
+                int groupStudents = students[i][j] + students[i + 1][j];
+                rooms += (groupStudents + K - 1) / K; // 올림 처리하여 필요한 방 수 계산
             }
         }
         
