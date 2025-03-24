a5e9bad5a71a0b00baf79c0906616f56c19e41d1
전상민 <95060314+jsangmin99@users.noreply.github.com>
2025-03-24 18:15:24 +0900

    [Bronze II] Title: 점심시간 레이스, Time: 316 ms, Memory: 37516 KB -BaekjoonHub

---
+++ 백준/Bronze/28236. 점심시간 레이스/점심시간 레이스.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int minTime = Integer.MAX_VALUE;
+        int resultBan = Integer.MAX_VALUE;
+
+        for(int i = 1; i <= k; i++){
+            st = new StringTokenizer(br.readLine());
+
+            int floor = Integer.parseInt(st.nextToken());
+            int ban = Integer.parseInt(st.nextToken());
+
+            int time = (floor - 1) + (m - ban) + 1;
+
+            if(time < minTime || (time == minTime && i < resultBan)){
+                minTime = time;
+                resultBan = i;
+            }
+        }
+        System.out.println(resultBan);
+    }
+}
