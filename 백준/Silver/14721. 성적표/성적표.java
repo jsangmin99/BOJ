236354b8f5caaa646a1a11826454566f407940e5
전상민 <95060314+jsangmin99@users.noreply.github.com>
2025-03-25 01:27:47 +0900

    [Silver V] Title: 성적표, Time: 140 ms, Memory: 16084 KB -BaekjoonHub

---
+++ 백준/Silver/14721. 성적표/성적표.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] x = new int[n];
+        int[] y = new int[n];
+
+        StringTokenizer st;
+        for(int i = 0 ; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+            x[i] = Integer.parseInt(st.nextToken());
+            y[i] = Integer.parseInt(st.nextToken());
+        }
+
+        long minRss = Long.MAX_VALUE;
+        int bestA = 0;
+        int bestB = 0;
+
+        for(int a = 1; a <= 100; a++){
+            for(int b = 1; b <=100; b++){
+                long rss =0;
+                for(int i = 0; i < n; i++){
+                    long fx = a * x[i] +b;
+                    long diff = y[i] -fx;
+                    rss += diff * diff;
+                }
+                if (rss < minRss){
+                    minRss = rss;
+                    bestA = a;
+                    bestB = b;
+                }
+            }
+        }
+        System.out.println(bestA + " " + bestB);
+    }
+    
+}
