a5cdf7bdc5c5ba0671aab737ec2871bdbc92d3aa
전상민 <95060314+jsangmin99@users.noreply.github.com>
2025-03-25 15:09:58 +0900

    [Silver III] Title: 놀라운 문자열, Time: 136 ms, Memory: 16436 KB -BaekjoonHub

---
+++ 백준/Silver/1972. 놀라운 문자열/놀라운 문자열.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while (!(line = br.readLine()).equals("*")) {
+            boolean isSurprising = true;
+            int len = line.length();
+
+            for (int d = 0; d < len - 1; d++) {
+                HashSet<String> pairs = new HashSet<>();
+                for (int i = 0; i + 1 + d < len; i++) {
+                    String pair = "" + line.charAt(i) + line.charAt(i + 1 + d);
+                    if (pairs.contains(pair)) {
+                        isSurprising = false;
+                        break;
+                    }
+                    pairs.add(pair);
+                }
+                if (!isSurprising)
+                    break;
+            }
+            if (isSurprising) {
+                System.out.println(line + " is surprising.");
+            } else {
+                System.out.println(line + " is NOT surprising.");
+            }
+
+        }
+
+    }
+
+}
