1d0f8937fd1c29f991f0c4a7f7ffe3d19381415f
전상민 <95060314+jsangmin99@users.noreply.github.com>
2025-04-04 23:16:04 +0900

    [Silver IV] Title: 2+1 세일, Time: 400 ms, Memory: 28812 KB -BaekjoonHub

---
+++ 백준/Silver/11508. 2＋1 세일/2＋1 세일.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Integer[] arr = new Integer[n];
+        for(int i = 0 ; i < n; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr, Collections.reverseOrder());
+        int total = 0;
+        for(int i = 0; i < n; i++){
+            if(i % 3 != 2){
+                total += arr[i];
+            }
+        }
+
+        System.out.println(total);
+
+    }
+    
+}
