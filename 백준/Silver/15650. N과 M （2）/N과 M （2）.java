182b564d7d21fe5929b1a59d09197b036a2b7eef
전상민 <95060314+jsangmin99@users.noreply.github.com>
2025-04-24 15:03:05 +0900

    [Silver III] Title: N과 M (2), Time: 196 ms, Memory: 18368 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.util.Scanner;
+
+public class Main {
+    static int N;
+    static int M;
+    static int[] result;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        result = new int[M];
+        dfs(1, 0);
+    }
+
+    static void dfs(int start, int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                System.out.print(result[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = start; i <= N; i++) {
+            result[depth] = i;
+            dfs(i + 1, depth + 1);
+        }
+    }
+}
