73f61cd59efedae38e01730dd1297ce0c8381c05
전상민 <95060314+jsangmin99@users.noreply.github.com>
2025-05-13 18:10:48 +0900

    [D3] Title: 24시간, Time: 96 ms, Memory: 26,752 KB -BaekjoonHub

---
+++ SWEA/D3/12368. 24시간/24시간.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        for(int i = 1; i <= T; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            int time = a+ b;
+            System.out.println("#" + i + " " + time % 24);
+        }
+    }
+    
+}
---
+++ SWEA/D3/12368. 24시간/README.md
@@ -0,0 +1,15 @@
+# [D3] 24시간 - 12368 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AXsEBlLqedsDFARX) 
+
+### 성능 요약
+
+메모리: 26,752 KB, 시간: 96 ms, 코드길이: 594 Bytes
+
+### 제출 일자
+
+2025-05-13 18:10
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
