634ec7e589e26b8600386c586caeea55204d15ba
전상민 <95060314+jsangmin99@users.noreply.github.com>
2025-05-13 18:20:40 +0900

    [D3] Title: 준환이의 운동관리, Time: 105 ms, Memory: 27,392 KB -BaekjoonHub

---
+++ SWEA/D3/3431. 준환이의 운동관리/준환이의 운동관리.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        for (int testCase = 1; testCase <= T; testCase++) {
+            st = new StringTokenizer(br.readLine());
+
+            long L = Long.parseLong(st.nextToken());
+            long U = Long.parseLong(st.nextToken());
+            long X = Long.parseLong(st.nextToken());
+
+            long result = 0;
+            if (X > U) {
+                result = -1;
+            } else if (X < L) {
+                result = L - X;
+            }else{
+                result = 0;
+            }
+            System.out.println("#" + testCase + " " + result);
+
+        }
+
+    }
+
+}
