72b6e0d7ff3def7741f56c863e955616bc125c50
전상민 <95060314+jsangmin99@users.noreply.github.com>
2025-05-16 17:46:12 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.72 ms, Memory: 56.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=java) 
 
 ### 성능 요약
 
-메모리: 4.02 MB, 시간: 0.23 ms
+메모리: 56.8 MB, 시간: 8.72 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 18일 11:15:31
+2025년 05월 16일 17:46:10
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,33 +1,40 @@
 import java.util.*;
-
 class Solution {
+    static int[] dx = new int[]{-1, 1, 0, 0};
+    static int[] dy = new int[]{0, 0, -1, 1};
+    
     public int solution(int[][] maps) {
         int answer = 0;
         int n = maps.length;
         int m = maps[0].length;
-        int[][] visited = new int[n][m];
-        int[] dx = {0, 0, 1, -1};
-        int[] dy = {1, -1, 0, 0};
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{0, 0});
-        visited[0][0] = 1;
+        boolean visited[][] = new boolean[n][m];
+        Queue<int[]> que = new LinkedList<>();
+        que.offer(new int[]{0, 0, 1}); // x, y, distance
+        visited[0][0] = true;
         
-        while (!q.isEmpty()) {
-            int[] now = q.poll();
+        while(!que.isEmpty()){
+            int[] now = que.poll();
             int x = now[0];
             int y = now[1];
-            for (int i = 0; i < 4; i++) {
+            int dist = now[2];
+            
+            for(int i = 0 ; i< 4; i++){
                 int nx = x + dx[i];
                 int ny = y + dy[i];
+                
+                // 목적지에 도달하면 종료
+                if (x == n - 1 && y == m - 1) {
+                    return dist;
+                }
+            
                 if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
-                    if (maps[nx][ny] == 1 && visited[nx][ny] == 0) {
-                        visited[nx][ny] = visited[x][y] + 1;
-                        q.offer(new int[]{nx, ny});
+                    if(maps[nx][ny] == 1 && !visited[nx][ny]){
+                        visited[nx][ny] = true;
+                        que.offer(new int[] {nx, ny, dist+1});
                     }
                 }
             }
         }
-        answer = visited[n - 1][m - 1] == 0 ? -1 : visited[n - 1][m - 1];
-        return answer;
+        return -1;
     }
 }
