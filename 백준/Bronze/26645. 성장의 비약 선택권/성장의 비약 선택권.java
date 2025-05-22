aba2578fbd8eba2c3ca8c54a1ac6a7d46357d064
전상민 <95060314+jsangmin99@users.noreply.github.com>
2025-05-22 15:59:35 +0900

    [Bronze III] Title: 성장의 비약 선택권, Time: 172 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/26645. 성장의 비약 선택권/성장의 비약 선택권.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        if (N < 206) {
+            System.out.println(1);
+        } else if ( N < 218) {
+            System.out.println(2);
+        } else if ( N < 229) {
+            System.out.println(3);
+        } else {
+            System.out.println(4);
+        }
+    }
+
+}
