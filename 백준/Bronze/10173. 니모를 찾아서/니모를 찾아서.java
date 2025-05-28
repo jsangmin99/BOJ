7e498b4d1e2910d80cc7b742df9933040f20e6f1
전상민 <95060314+jsangmin99@users.noreply.github.com>
2025-05-28 19:16:35 +0900

    [Bronze II] Title: 니모를 찾아서, Time: 104 ms, Memory: 13984 KB -BaekjoonHub

---
+++ 백준/Bronze/10173. 니모를 찾아서/니모를 찾아서.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            String line = br.readLine();
+            if (line.equals("EOI")) {
+                break;
+            }
+            if (line.toLowerCase().contains("nemo")) {
+                System.out.println("Found");
+            } else {
+                System.out.println("Missing");
+            } 
+            
+        }
+    }
+}
